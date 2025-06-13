25c4e72d273d8e36df1c2f25ae570b54034a67b9
Abhinav-0103 <abhinava808@gmail.com>
2025-06-13 19:07:12 +0530

    Data Setup

--- app.py
+++ app.py
@@ -1,56 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from utils.mail import log_to_excel
+import pandas as pd
 
 app = Flask(__name__, template_folder="templates", static_folder="static", static_url_path="/")
 app.secret_key = 'your_secret_key'  # for flashing messages
 
 # Projects sample data
-projects = [
-    {
-        'title': 'Titanic Survival Prediction',
-        'description': 'A classification model using logistic regression and XGBoost.',
-        'link': '#'
-    },
-    {
-        'title': 'Image Classifier',
-        'description': 'Deep learning CNN model for classifying images using PyTorch.',
-        'link': '#'
-    },
-    {
-        'title': 'Titanic Survival Prediction',
-        'description': 'A classification model using logistic regression and XGBoost.',
-        'link': '#'
-    },
-    {
-        'title': 'Image Classifier',
-        'description': 'Deep learning CNN model for classifying images using PyTorch.',
-        'link': '#'
-    },
-    {
-        'title': 'Titanic Survival Prediction',
-        'description': 'A classification model using logistic regression and XGBoost.',
-        'link': '#'
-    },
-    {
-        'title': 'Image Classifier',
-        'description': 'Deep learning CNN model for classifying images using PyTorch.',
-        'link': '#'
-    },
-    {
-        'title': 'Titanic Survival Prediction',
-        'description': 'A classification model using logistic regression and XGBoost.',
-        'link': '#'
-    },  
-    {
-        'title': 'Image Classifier',
-        'description': 'Deep learning CNN model for classifying images using PyTorch.',
-        'link': '#'
-    }
-]
+projects = pd.read_json("projects.json").values.tolist()
+skills = pd.read_json("skills.json").values.tolist()
 
 @app.route('/')
 def home():
-    return render_template('base.html', projects=projects)
+    return render_template('base.html', projects=projects, skills=skills)
 
 @app.route('/contact', methods=['POST'])
 def contact():
